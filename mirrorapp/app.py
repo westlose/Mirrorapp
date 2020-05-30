8610a021ca974b09aeb98f560a41bb4eabf4a554
westlose <anamir94@gmail.com>
2020-05-30 12:18:11 +0200

    Add files via upload

---
+++ mirrorapp/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/news')
+def cakes():
+    return render_template('news.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
